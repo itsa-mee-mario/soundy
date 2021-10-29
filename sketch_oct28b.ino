e1480f524eb9367175c53433970d2c4f4a4d562a
itsa-mee-mario <91111595+itsa-mee-mario@users.noreply.github.com>
2021-10-29 13:02:53 +0530

    made this much better by adding a switch case instead of multiple ifs

--- sketch_oct28b.ino
+++ sketch_oct28b.ino
@@ -5,7 +5,7 @@ void setup() {
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
-  Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
+  Serial.begin(2000000); 
         digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
@@ -13,64 +13,54 @@ void setup() {
 }
 
 void loop() {
+  
+
 
 
   // send data only when you receive data:
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
+    
 
-    Serial.print("I received: ");
+   
     Serial.println(incomingByte);
 
-
-    if (incomingByte == 'l' ) {
-     
+        
+  switch (incomingByte) {
+  case 'l':
+        digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);
       digitalWrite(5, LOW);
       Serial.println("low");
-     
-
-    }
-
-    if (incomingByte == 'm' ) {
-  
+    break;
+  case 'm':
+        digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);
       digitalWrite(5, LOW);
       digitalWrite(6, LOW);
-
       Serial.println("med");
-  
-
-
-    }
-
-    if (incomingByte == 'h' ) {
-
+    break;
+  case 'h':
       digitalWrite(5, LOW);
       digitalWrite(6, LOW);
       digitalWrite(7, LOW);
       digitalWrite(8, LOW);
-      Serial.println("high");
-
-
-
-
-    }
-
-
-
-    if (incomingByte == 'd' ) {
-          
+      Serial.println("high");   
+      break;
+  case 'd':
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
       digitalWrite(8, HIGH);
       Serial.println("down");
+  default:
+    break;
 
- 
-
-
-
-    }
-
+  }
   }
 }
